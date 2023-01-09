fdd8bad1cad615d34a001f34e5c484c97d36f959
unknown <3003abhishek.jha@gmail.comgit config --global user.name Abhishek>
2023-01-09 11:04:16 +0530

    deployment

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function Navbar(){
     return(
         <div>
-          <Link href="/counter">Counter</Link>
+          <Link href="/counter">Counter1</Link>
 
         </div>
     )
