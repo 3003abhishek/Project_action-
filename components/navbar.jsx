9bc755ee49d5a8f83f08fd41a387bcca2cebc042
unknown <3003abhishek.jha@gmail.comgit config --global user.name Abhishek>
2023-01-09 09:06:23 +0530

    first post

---
+++ components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link"
+function Navbar(){
+    return(
+        <div>
+          <Link href="/counter">Counter</Link>
+
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ pages/counter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Counter(){
+    return(
+        <>
+        <h1>count</h1>
+        </>
+    )
+}
+export default Counter;
